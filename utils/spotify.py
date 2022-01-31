bf26f9100700376e20b534effe771bdc21657bf7
alt-qi <a-ti-radeon@yandex.ru>
2022-01-31 13:32:34 +0700

    Edited Spotify scopes and requirements

--- utils/spotify.py
+++ utils/spotify.py
@@ -9,7 +9,7 @@ client_id = getenv("spotify_client_id")
 client_secret = getenv("spotify_client_secret")
 redirect_uri = getenv("redirect_uri")
 
-oauth = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri, scope="user-read-currently-playing")
+oauth = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri, scope="user-read-currently-playing user-read-recently-played")
 
 def get_token(code: str):
     return oauth.get_access_token(code)['access_token']
